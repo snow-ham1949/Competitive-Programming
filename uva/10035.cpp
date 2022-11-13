09cf78fcb4ea959fd42d880567a6a870ff382609
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-11-13 23:42:40 +0800

    add 10035

---
+++ uva/10035.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define SZ(a) (int)(a).size()
+#define endl '\n'
+#define X first
+#define Y second
+
+void solve()
+{
+  ll n, m;
+  while (scanf("%lld%lld", &n, &m) == 2 && n != 0 && m != 0) {
+    int carry = 0, cnt = 0;
+    while (n != 0 || m != 0) {
+      if (n % 10 + m % 10 + carry >= 10) {
+        carry = 1;
+        cnt++;
+      } else {
+        carry = 0;
+      }
+      n /= 10;
+      m /= 10;
+    }
+    if (cnt == 0) {
+      printf("No carry operation.\n");
+    }
+    else if (cnt == 1) {
+      printf("%d carry operation.\n", cnt);
+    }
+    else {
+      printf("%d carry operations.\n", cnt);
+    }
+  }
+}
+
+int main() {
+  //ios::sync_with_stdio(0);cin.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve(); 
+  }
+  return 0;
+}
+
