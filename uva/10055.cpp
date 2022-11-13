b12d64b5c604e2551c2756a0ec1dfef8eeec950e
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-11-13 20:01:38 +0800

    add 10055

---
+++ uva/10055.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define SZ(a) (int)(a).size()
+#define endl '\n'
+#define X first
+#define Y second
+
+void solve()
+{
+  ll n, m;
+  while (cin >> n >> m) {
+    cout << abs(n - m) << endl;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0);cin.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve(); 
+  }
+  return 0;
+}
+
