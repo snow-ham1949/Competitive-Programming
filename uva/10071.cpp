256fb32b710e74be46fadc3cf1fc9c6ae5af2144
李芸芳 <b08502041@csie.ntu.edu.tw>
2022-12-09 00:32:24 +0800

    add 10071

---
+++ uva/10071.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define SZ(a) (int)(a).size()
+#define X first
+#define Y second
+
+int v, t;
+
+void solve()
+{
+  cout << 2 * v * t << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(0);cin.tie(0);
+  int T = 1;
+  // cin >> T;
+  // for (int i = 1; i <= T; i++)
+  while (cin >> v >> t)
+  {
+    solve(); 
+  }
+  return 0;
+}
+
