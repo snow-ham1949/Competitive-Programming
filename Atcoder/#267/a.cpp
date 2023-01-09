356702fb90ff4cdb51f19f448d59e103b5c1167c
李芸芳 <b08502041@csie.ntu.edu.tw>
2023-01-09 23:02:13 +0800

    remove pb_ds

--- Atcoder/#267/a.cpp
+++ Atcoder/#267/a.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
 
-using namespace __gnu_pbds;
+// using namespace __gnu_pbds;
 using namespace std;
 
-template<class T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<class T>
-using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template<class T>
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template<class T>
+// using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<class T> bool chmax(T &a, T b) { return a < b && (a = b, true); }
 template<class T> bool chmin(T &a, T b) { return a > b && (a = b, true); }
 
